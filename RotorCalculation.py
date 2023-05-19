42f4bb160b18593f0fbf7a9e7f96cb68a00a333b
Bart <tychnor@gmail.com>
2023-05-19 23:36:14 +0200

    refactoring

--- RotorCalculation.py
+++ RotorCalculation.py
@@ -73,20 +73,20 @@ class CalculationOfRotor:
         return exc_x_rot, exc_y_rot, res_x_rot, res_y_rot
 
     def __calculate_one_cycle_of_jeff_linear_rotor_sym(self, T, dt, X, SDM, Hz, mass_eccentricity):
-        self.cal_new_poz(X, T, dt, Hz, mass_eccentricity)
-        self.cal_new_velocity(X, T, dt, Hz, mass_eccentricity)
+        self.__cal_new_poz(X, T, dt, Hz, mass_eccentricity)
+        self.__cal_new_velocity(X, T, dt, Hz, mass_eccentricity)
 
         # s - stiffness
-        self.cal_new_s_forces_n(X, SDM)
+        self.__cal_new_s_forces_n(X, SDM)
         # d - damping
-        self.cal_new_d_forces(X, SDM)
+        self.__cal_new_d_forces(X, SDM)
 
         # a - acceleration
-        self.cal_new_a_forces(X, SDM)
+        self.__cal_new_a_forces(X, SDM)
         # f - force
-        self.cal_new_f_sum(X)
+        self.__cal_new_f_sum(X)
 
-        self.cal_new_accelerations(X, SDM, T, dt, Hz, mass_eccentricity)
+        self.__cal_new_accelerations(X, SDM, T, dt, Hz, mass_eccentricity)
 
     @staticmethod
     def __calculate_phase_angle(T, Hz):
@@ -97,15 +97,15 @@ class CalculationOfRotor:
         return ecc * np.cos(2 * np.pi * Hz * T), ecc * np.sin(2 * np.pi * Hz * T)
 
     @staticmethod
-    def calc_velocity_of_exc(T, Hz, ecc):
+    def __calc_velocity_of_exc(T, Hz, ecc):
         return -ecc * 2 * np.pi * Hz * np.sin(2 * np.pi * Hz * T), ecc * 2 * np.pi * Hz * np.cos(2 * np.pi * Hz * T)
 
     @staticmethod
-    def calc_acceleration_of_exc(T, Hz, ecc):
+    def __calc_acceleration_of_exc(T, Hz, ecc):
         return -ecc * (2 * np.pi * Hz)**2 * np.cos(2 * np.pi * Hz * T),\
                -ecc * (2 * np.pi * Hz)**2 * np.sin(2 * np.pi * Hz * T)
 
-    def cal_new_poz(self, X, T, dt, Hz, ecc):
+    def __cal_new_poz(self, X, T, dt, Hz, ecc):
         # calc new position of excitation
         X[0][0][0], X[0][0][1] = self.__calc_position_of_exc(T, Hz, ecc)
 
@@ -113,24 +113,24 @@ class CalculationOfRotor:
         X[0][1][0] = X[0][1][0] + X[1][1][0] * dt
         X[0][1][1] = X[0][1][1] + X[1][1][1] * dt
 
-    def cal_new_velocity(self, X, T, dt, Hz, ecc):
+    def __cal_new_velocity(self, X, T, dt, Hz, ecc):
         # calc new velocity of excitation
-        X[1][0][0], X[1][0][1] = self.calc_velocity_of_exc(T, Hz, ecc)
+        X[1][0][0], X[1][0][1] = self.__calc_velocity_of_exc(T, Hz, ecc)
 
         # calc new velocity of response
         X[1][1][0] = X[1][1][0] + X[2][1][0] * dt
         X[1][1][1] = X[1][1][1] + X[2][1][1] * dt
 
-    def cal_new_accelerations(self, X, SDM, T, dt, Hz, ecc):
+    def __cal_new_accelerations(self, X, SDM, T, dt, Hz, ecc):
         # excitation
-        X[2][0][0], X[2][0][1] = self.calc_acceleration_of_exc(T, Hz, ecc)
+        X[2][0][0], X[2][0][1] = self.__calc_acceleration_of_exc(T, Hz, ecc)
 
         # response
         X[2][1][0] = X[3][1][0]/SDM[3]
         X[2][1][1] = X[3][1][1]/SDM[3]
 
     @staticmethod
-    def cal_new_s_forces_n(X, SDM):
+    def __cal_new_s_forces_n(X, SDM):
         # stiffness forces acting on excitation
         X[5][0][0] = SDM[0] * X[0][1][0]
         X[5][0][1] = SDM[0] * X[0][1][1]
@@ -139,17 +139,17 @@ class CalculationOfRotor:
         X[5][1][1] = -SDM[0] * X[0][1][1]
 
     @staticmethod
-    def cal_new_d_forces(X, SDM):
+    def __cal_new_d_forces(X, SDM):
         X[6][1][0] = -SDM[2] * X[1][1][0]
         X[6][1][1] = -SDM[2] * X[1][1][1]
 
     @staticmethod
-    def cal_new_a_forces(X, SDM):
+    def __cal_new_a_forces(X, SDM):
         X[4][1][0] = -SDM[3] * X[2][0][0]
         X[4][1][1] = -SDM[3] * X[2][0][1]
 
     @staticmethod
-    def cal_new_f_sum(X):
+    def __cal_new_f_sum(X):
         # sum for excitation
         X[3][0][0] = X[4][0][0] + X[5][0][0] + X[6][0][0]
         X[3][0][1] = X[4][0][1] + X[5][0][1] + X[6][0][1]
